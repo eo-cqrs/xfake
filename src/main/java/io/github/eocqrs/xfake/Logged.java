f71ce139088fe64135350a9d9916c4dea2f4f783
h1alexbel <hialexbel@gmail.com>
2023-05-29 11:19:23 +0300

    #28

---
+++ src/main/java/io/github/eocqrs/xfake/Logged.java
@@ -0,0 +1,49 @@
+package io.github.eocqrs.xfake;
+
+import com.jcabi.log.Logger;
+import com.jcabi.xml.XML;
+import org.xembly.Directive;
+
+import java.util.logging.Level;
+
+/**
+ * Logging decorator for {@link FkStorage}.
+ *
+ * @author Aliaksei Bialiauski (abialiauski.dev@gmail.com)
+ * @since 0.1.2
+ */
+public final class Logged implements FkStorage {
+
+  /**
+   * Origin Storage to decorate.
+   */
+  private final FkStorage origin;
+  /**
+   * Logging level.
+   */
+  private final Level level;
+
+  /**
+   * Ctor.
+   *
+   * @param orgn Storage
+   */
+  public Logged(final FkStorage orgn, final Level lvl) {
+    this.origin = orgn;
+    this.level = lvl;
+  }
+
+  @Override
+  public XML xml() throws Exception {
+    Logger.log(this.level, this, "#xml method was called");
+    final XML xml = this.origin.xml();
+    Logger.log(this.level, this, "XML loaded: %s", xml);
+    return xml;
+  }
+
+  @Override
+  public void apply(final Iterable<Directive> dirs) throws Exception {
+    Logger.log(this.level, this, "#apply(%s) was called", dirs);
+    this.origin.apply(dirs);
+  }
+}
---
+++ src/test/java/io/github/eocqrs/xfake/LoggedTest.java
@@ -0,0 +1,43 @@
+package io.github.eocqrs.xfake;
+
+import com.jcabi.log.Logger;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.xembly.Directives;
+
+import java.util.logging.Level;
+
+/**
+ * Test case for {@link Logged}.
+ *
+ * @author Aliaksei Bialiauski (abialiauski.dev@gmail.com)
+ * @since 0.0.2
+ */
+final class LoggedTest {
+
+  @Test
+  void writesXmlLog() throws Exception {
+    final Level lvl = Level.INFO;
+    final FkStorage storage =
+      new Logged(
+        new Synchronized(
+          new InFile("logged-test", "<fake/>")
+        ),
+        lvl
+      );
+    Assertions.assertDoesNotThrow(storage::xml);
+    Assertions.assertDoesNotThrow(() -> storage.apply(
+        new Directives()
+          .xpath("/fake")
+          .addIf("ping")
+      )
+    );
+    MatcherAssert.assertThat(
+      "Logging is enabled",
+      Logger.isEnabled(lvl, Logged.class),
+      Matchers.is(true)
+    );
+  }
+}
